13da915b6997bd2fe8109b32269087f3bf691d85
Matthes-Baer <Matthes.baer@yahoo.de>
2022-03-28 23:41:18 +0200

    feat: added random Numbers
    Added random Numbers from array for "Games Today" and "Playing Now".

--- JS/jQuery.js
+++ JS/jQuery.js
@@ -19,4 +19,12 @@ $(function() {
     function mediaQueryFunction(){
         $('.asideFlexContainer').css('display', 'block');
     }
+
+    const randomGamesToday = ["9,602,102", "8,991,301", "7,909,911", "10,301,102", "9,335,023"];
+    const randomPlayingNow = ["187,301", "301,222", "219,401", "199,230", "255,552"];
+    // IIFE for random "Games Today" and random "Playing Now"
+    (function () {
+        $('#gamesToday').text(randomGamesToday[Math.floor(Math.random() * randomGamesToday.length)]);
+        $('#playingNow').text(randomPlayingNow[Math.floor(Math.random() * randomPlayingNow.length)]);
+    })();
 });
